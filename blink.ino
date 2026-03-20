3cb551c2710c8da189de8b7e295eb7e47ca42ea4
ingrid <132859781+infriglide@users.noreply.github.com>
2026-03-20 11:11:26 -0300

    Update semaforo

--- blink.ino
+++ blink.ino
@@ -1,4 +1,4 @@
-/*
+*
   Blink
 
   Turns an LED on for one second, then off for one second, repeatedly.
@@ -27,14 +27,20 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(8, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(4, OUTPUT);
+  
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(8, LOW);
-  delay(1000);                       // wait for a second
+  delay(10000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(8, HIGH);
-  delay(1000);                       // wait for a second
+  digitalWrite(13, HIGH);
+  delay(5000);             
+  digitalWrite(4, HIGH);
+  delay(2000);                      
+
 }
